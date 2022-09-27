e980961c376d6120893a8dceedf232407879c9e1
sorlaw <putrajangjayakoncoro@gmail.com>
2022-09-27 13:32:49 +0700

    pwpb_270922_basic JS chapter 1

---
+++ mathOperator_4_incrementDecrement.js
@@ -0,0 +1,27 @@
+// increment
+console.log("==========increment==========");
+var a = 0;
+var b = 10;
+var a = b + 2;
+console.log(b);
+console.log(a);
+
+var c = 0;
+var d = 10;
+var c = (d += 2);
+console.log(c);
+console.log(d);
+
+// decrement
+console.log("==========decrement==========");
+var e = 0;
+var f = 10;
+console.log(f);
+var e = (f -= 2);
+console.log(e);
+
+var g = 0;
+var h = 10;
+var g = (h -= 2);
+console.log(g);
+console.log(h);
